27bfb8329bc9e5e99948d41b0bc6ded8cb2fff64
Jin <18kimj89@millshighsch.org>
2017-08-24 09:30:03 -0700

    first one

---
+++ Project_2/src/Project_2.java
@@ -0,0 +1,12 @@
+//Jin Kim//
+/*August 23, 2017
+Project 2 -- Use print and println*/
+public class Project_2 {
+
+	public static void main(String[] args) {
+	System.out.println("hi there");
+	System.out.println("hi there");
+	System.out.print("hello");
+	}
+
+}
