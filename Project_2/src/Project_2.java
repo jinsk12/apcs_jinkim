69df9033c0ce34144284589f59ff260ade289bf2
Jin <18kimj89@millshighsch.org>
2017-08-30 10:34:56 -0700

    new pikachu

--- Project_2/src/Project_2.java
+++ Project_2/src/Project_2.java
@@ -4,8 +4,8 @@ Project 2 -- Use print and println*/
 public class Project_2 {
 
 	public static void main(String[] args) {
-	System.out.println("hi there");
-	System.out.println("hi there");
+	System.out.print("hi there");
+	System.out.print("hi there");
 	System.out.print("hi");
 	System.out.print("hello");
 	}
