2be474764129bbe299b0930a29311a6479f2d546
Jin <18kimj89@millshighsch.org>
2017-08-30 10:42:27 -0700

    updated pikachu

---
+++ Pikachu/src/Pikachu.java
@@ -0,0 +1,12 @@
+//Pikachu Project//
+//August 30, 2017//
+public class Pikachu {
+
+	public static void main(String[] args) {
+System.out.println("Pikachu welcomes you to the world of Pokemon!");
+System.out.println("\t \t(\\__/)");
+System.out.println("\t \t(o^.^)");
+System.out.println("\t \tz(_(\")(\")");
+	}
+
+}
