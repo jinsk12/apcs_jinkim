f27924054bf7835cdedcd8434b212e11435b1ab3
Jin <31106980+saucyjin@users.noreply.github.com>
2017-09-25 09:10:32 -0700

    Update Calculate.java
    average changes

--- Calculate Library/src/Calculate.java
+++ Calculate Library/src/Calculate.java
@@ -13,14 +13,14 @@ public class Calculate {
 			return cube;
 		}
 //Average For Two Doubles
-		public static double average2(double operand3, double operand4) {
-			double average2 = (operand3+operand4)/2;
-			return average2;
+		public static double average(double operand3, double operand4) {
+			double average = (operand3+operand4)/2;
+			return average;
 		}
 //Average For Three Doubles
-		public static double average3(double operand5, double operand6, double operand7) {
-			double average3 = (operand5+operand6+operand7)/3;
-			return average3;
+		public static double average(double operand5, double operand6, double operand7) {
+			double average = (operand5+operand6+operand7)/3;
+			return average;
 		}
 //Change to Degrees
 		public static double toDegrees(double operand8) {
